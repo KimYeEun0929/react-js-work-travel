f96afa1d4d5d31d2b8b3561fa83ccd8dc59d601d
nabi1110 <nabinabi1110@gmail.com>
2024-08-12 21:51:20 +0900

    work to do and travel to do

--- App.js
+++ App.js
@@ -1,11 +1,130 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+  ScrollView,
+  Alert,
+} from "react-native";
+import { theme } from "./colors";
+import { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+const STORAGE_KEY = "@toDos";
 
 export default function App() {
+  const [working, setWorking] = useState(true);
+  const [text, setText] = useState("");
+  const [toDos, setToDos] = useState({}); //hashmap 같은 것을 만들 것임. array가 아니라 object(key와 value)
+
+  //앱을 처음 실행할 때 toDos들이 로드됨.
+  useEffect(() => {
+    loadToDos(STORAGE_KEY);
+  }, []);
+
+  const travel = () => setWorking(false); //여행탭은 false
+  const work = () => setWorking(true); //일 탭은 true
+  const onChangeText = (payload) => setText(payload); //payload는 input에 쓴 나의 글
+
+  const saveToDos = async (toSave) => {
+    try {
+      const s = JSON.stringify(toSave);
+      await AsyncStorage.setItem(STORAGE_KEY, s);
+    } catch (e) {
+      alert("save error");
+    }
+  };
+  const loadToDos = async (STORAGE_KEY) => {
+    try {
+      const s = await AsyncStorage.getItem(STORAGE_KEY);
+      setToDos(JSON.parse(s));
+    } catch (e) {
+      alert("load error");
+    }
+  };
+
+  const addToDo = async () => {
+    if (text === "") {
+      return;
+    }
+    //save to do
+    const newToDos = { ...toDos, [Date.now()]: { text, working } };
+    setToDos(newToDos);
+    await saveToDos(newToDos);
+    //input란 비우기
+    setText("");
+  };
+  //console.log(toDos);
+
+  const deleteToDo = (key) => {
+    Alert.alert("Delete To Do", "Are you sure?", [
+      {
+        text: "Delete",
+        onPress: () => {
+          const newToDos = { ...toDos };
+          delete newToDos[key];
+          setToDos(newToDos);
+          saveToDos(newToDos);
+        },
+      },
+      { text: "Cancel" },
+    ]);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <View style={styles.header}>
+        {/* Work */}
+        <TouchableOpacity onPress={work}>
+          <Text
+            style={{ ...styles.btnText, color: working ? "white" : theme.grey }}
+          >
+            Work
+          </Text>
+        </TouchableOpacity>
+
+        {/* Travel */}
+        <TouchableOpacity onPress={travel}>
+          <Text
+            style={{
+              ...styles.btnText,
+              color: !working ? "white" : theme.grey,
+            }}
+          >
+            Travel
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      {/* Input */}
+      <TextInput
+        value={text}
+        onChangeText={onChangeText}
+        onSubmitEditing={addToDo}
+        returnKeyType="done"
+        placeholder={working ? "Add a new To Do" : "Where do you want to go?"}
+        style={styles.input}
+      />
+
+      {/* ToDo */}
+      <ScrollView>
+        {/* Object.keys(x) : x의 key들을 배열로 나타냄. */}
+        {Object.keys(toDos).map((key) =>
+          toDos[key].working === working ? (
+            <View style={styles.toDo}>
+              <Text style={styles.toDoText}>{toDos[key].text}</Text>
+              <TouchableOpacity onPress={() => deleteToDo(key)}>
+                <Ionicons name="trash" size={24} color="white" />
+              </TouchableOpacity>
+            </View>
+          ) : null
+        )}
+      </ScrollView>
     </View>
   );
 }
@@ -13,8 +132,42 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 20,
+    backgroundColor: theme.bg,
+  },
+  header: {
+    flexDirection: "row",
+    marginTop: 100,
+    justifyContent: "space-between",
+    //backgroundColor: "tomato",
+  },
+  btnText: {
+    fontSize: 38,
+    fontWeight: "600",
+    //backgroundColor: "green",
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginVertical: 20,
+    backgroundColor: "white",
+    fontSize: 18,
+  },
+  toDo: {
+    backgroundColor: theme.grey,
+    marginBottom: 10,
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    borderRadius: 15,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  toDoText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "500",
+    //backgroundColor: "green",
   },
 });
